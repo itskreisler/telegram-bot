2a0aa1d8cb1e1d940dd312429c6100c448896e4f
Kreisler Ramirez <itskreisler@outlook.com>
2022-11-25 11:52:13 -0500

    add script sh

---
+++ runbot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while :
+do
+    rand=$RANDOM
+    dt=$(date '+%d/%m/%Y %H:%M:%S');
+    printf "${dt} - Init: ${rand}\n"
+    echo "Starting NODE bot"
+    node bot.js #npm run dev
+    echo "Script terminated, restarting in 2 minutes..."
+    dt=$(date '+%d/%m/%Y %H:%M:%S');
+    printf "${dt} - finished: ${rand}\n"
+    sleep 120
+    clear
+done
