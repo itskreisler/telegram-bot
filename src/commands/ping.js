fbfa191f7720e3feadd4efe9f7c688390c485f3a
Kreisler Ramirez <itskreisler@outlook.com>
2023-01-28 15:32:06 -0500

    fix

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,20 @@
+/* eslint-disable camelcase */
 import { bot } from '../bot.js'
 
 const cmdPingRegExp = /^\/ping/
-
+const parse_mode = 'Markdown'
 const cmdPingFn = (msg) => {
+  const start = new Date()
   const chatId = msg.chat.id
-  bot.sendMessage(chatId, 'pong')
+  bot.sendMessage(chatId, '*Pinging...*', { parse_mode }).then(sent => {
+    const end = new Date()
+    const ping = end - start
+    bot.editMessageText(`*Pong! Latency is* \`${ping}ms\``, {
+      chat_id: chatId,
+      message_id: sent.message_id,
+      parse_mode
+    })
+  })
 }
 
 export { cmdPingFn, cmdPingRegExp }
